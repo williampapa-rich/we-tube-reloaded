b5e20b8eda032277013fe6a5e1618dcc5d1b62df
William <william@Williamui-iMac.local>
2023-11-17 16:07:01 +0900

    3.4강까지 들음

--- src/server.js
+++ src/server.js
@@ -4,6 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port ${PORT} 🚀`);
 
