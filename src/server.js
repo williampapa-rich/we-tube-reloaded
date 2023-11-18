841c71350f188f53be5145c3eec6a00847de70b1
William <william@Williamui-iMac.local>
2023-11-18 22:17:54 +0900

    3.11강까지 들음

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
 };
 
-const handleHome = (req, res, next) => {
-  return res.send("I love middleware!");
-};
-
-app.get("/", logger, handleHome);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port ${PORT} 🚀`);
