3ea75670f2129d9c3301375184f06b67393b7e1d
William <william@Williamui-iMac.local>
2023-11-28 00:18:20 +0900

    6.3강까지 들음

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,12 +29,19 @@ export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
