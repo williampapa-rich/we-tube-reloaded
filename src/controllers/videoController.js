cdcd8a23ab0d792928c1ac8499819d5baa1b83f9
William <william@Williamui-iMac.local>
2023-12-01 21:16:35 +0900

    6.13강까지 들음

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,20 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
-
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -51,14 +23,6 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createdAt: "Just Now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+  const { title } = req.body;
   return res.redirect("/");
 };
---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port ${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
